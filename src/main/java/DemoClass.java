14bb3fad9194af5b33f54566cfce1f48ea382dc2
okgoogle3 <halalah.han@gmail.com>
2022-07-06 13:46:45 +0300

    2.1

--- src/main/java/DemoClass.java
+++ src/main/java/DemoClass.java
@@ -6,25 +6,3 @@ public class DemoClass {
     private Integer b = 20;
 
 }
-
-/*public class SumAnalyzer {
-    public void parse(Class<?> clazz)  {
-
-        Method[] methods = clazz.getMethods();
-        Field[] fields = clazz.getDeclaredFields();
-        int sum = 0;
-
-        for (Field field : fields) {
-            if (field.isAnnotationPresent(Sum.class)) {
-                try {
-                    field.invoke(null);
-                    pass++;
-                } catch (Exception e) {
-                    fail++;
-                }
-            }
-        }
-
-
-    }
-}*/
